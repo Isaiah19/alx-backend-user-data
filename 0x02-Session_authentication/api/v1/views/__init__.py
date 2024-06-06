8635c63db7d08f2c8cfaae88d010f111f1dd3f3e
Isaiah19 <nwezeifeanyi93@gmail.com>
2024-06-06 22:52:01 +0000

    0x02-Session_authentication

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, jsonify
+#!/usr/bin/env python3
+""" DocDocDocDocDocDoc
+"""
+from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Example view
-@app_views.route('/status', methods=['GET'])
-def status():
-    return jsonify({"status": "OK"})
+from api.v1.views.index import *
+from api.v1.views.users import *
+from api.v1.views.session_auth import *
 
+User.load_from_file()
---
+++ 0x02-Session_authentication/models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" module doc str
+"""
+from models.user import User
+
+
+User.load_from_file()
